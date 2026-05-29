586ff2159066dc3df0f0b872fc921a09e9fa98b2
giacomo-c <giacomo.carissimi@protonmail.com>
2026-05-29 15:44:34 -0400

    chore:  add dnf option to skip missing packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf install -y \
+dnf install -y --skip-unavailable\
     gtk-murrine-engine \
     gnome-themes-extra \
     sassc
